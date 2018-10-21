32fae8982c24df1d0d7d88ef371accc4132b597b
Ian Veldhuizen <36740749+IanVeldhuizen@users.noreply.github.com>
2018-10-21 15:21:02 +0100

    Finished styles and started js functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
-// You can use either a string for the player ID (i.e., `player`), 
-	// or `document.querySelector()` for any selector
-	var player = new MediaElementPlayer('player', {
-		pluginPath: "/path/to/shims/",
-	// When using `MediaElementPlayer`, an `instance` argument
-	// is available in the `success` callback
-		success: function(mediaElement, originalNode, instance) {
-			// do things
-		}
-	});
+
+const myVideo = document.querySelectorAll('video');
+let myCaptions = document.getElementsByTagName('span');
+
+
+myVideo.ontimeupdate = () => {
+    for (let i = 0; i = myCaptions.length; i++){
+            let timeStamp = myVideo.getCurrentTime();
+            let timeStart = myCaptions[i].getAttribute('data-start');
+            let timeEnd = myCaptions[i].getAttribute('data-end');
+                if (timeStamp > timeStart & timeStamp < timeEnd){
+                    myCaptions[i].style.color = "black";
+                } else {
+                    myCaptions[i].style.color = "";
+                }
+        }
+};
+
+// Highlight captions===========================
+
+// myVideo.addEventListener("ontimeupdate", () => {
+
+// for (let i = 0; i = myCaptions.length; i++){
+//     myVideo.getCurrentTime();
+//     let timeStart = myCaptions[i].getAttribute('data-start');
+//     let timeEnd = myCaptions[i].getAttribute('data-end');
+//         if (myVideo > timeStart && myVideo < timeEnd){
+//             myCaptions[i].style.color = "black";
+//         } else {
+//             myCaptions[i].style.color = "null";
+//         }
+// }
+
+// });
+
+
+
+
+
+
+//   // the video players jumps to the appropriate time when the user clicks on any sentence in the transcript
+//   for (let i = 0; i < captions.length; i += 1){
+// 	captions[i].addEventListener('click', (e) => {
+// 		let dataStart = e.target.getAttribute("data-start");
+// 		video.setCurrentTime(dataStart);
+// 		video.play();
+// 	});
+//   }
