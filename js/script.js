9a6a1db8a90a7c9bcd412dc7c4354c40edb0d8ec
Ian Veldhuizen <36740749+IanVeldhuizen@users.noreply.github.com>
2018-10-22 13:07:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-
 const myVideo = document.querySelectorAll('video');
-let myCaptions = document.getElementsByTagName('span');
+let myCaptions = document.querySelectorAll('p .text');
 
 
 myVideo.ontimeupdate = () => {
@@ -15,34 +14,3 @@ myVideo.ontimeupdate = () => {
                 }
         }
 };
-
-// Highlight captions===========================
-
-// myVideo.addEventListener("ontimeupdate", () => {
-
-// for (let i = 0; i = myCaptions.length; i++){
-//     myVideo.getCurrentTime();
-//     let timeStart = myCaptions[i].getAttribute('data-start');
-//     let timeEnd = myCaptions[i].getAttribute('data-end');
-//         if (myVideo > timeStart && myVideo < timeEnd){
-//             myCaptions[i].style.color = "black";
-//         } else {
-//             myCaptions[i].style.color = "null";
-//         }
-// }
-
-// });
-
-
-
-
-
-
-//   // the video players jumps to the appropriate time when the user clicks on any sentence in the transcript
-//   for (let i = 0; i < captions.length; i += 1){
-// 	captions[i].addEventListener('click', (e) => {
-// 		let dataStart = e.target.getAttribute("data-start");
-// 		video.setCurrentTime(dataStart);
-// 		video.play();
-// 	});
-//   }
