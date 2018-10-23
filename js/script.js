49f343dcfb8b7c3b5d1b2e2635ffe5a2adf558f5
Ian Veldhuizen <36740749+IanVeldhuizen@users.noreply.github.com>
2018-10-23 20:09:38 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-const myVideo = document.querySelectorAll('video');
-let myCaptions = document.querySelectorAll('p .text');
+document.addEventListener("DOMContentLoaded", function() { 
 
+    const myVideo = document.querySelector('video');
+    let myCaptions = document.querySelectorAll('p .text');
 
-myVideo.ontimeupdate = () => {
-    for (let i = 0; i = myCaptions.length; i++){
-            let timeStamp = myVideo.getCurrentTime();
-            let timeStart = myCaptions[i].getAttribute('data-start');
-            let timeEnd = myCaptions[i].getAttribute('data-end');
-                if (timeStamp > timeStart & timeStamp < timeEnd){
-                    myCaptions[i].style.color = "black";
-                } else {
-                    myCaptions[i].style.color = "";
-                }
-        }
-};
+// Highlight captions===========================
+
+    myVideo.ontimeupdate = () => {
+        for (let i = 0; i < myCaptions.length; i++){
+                let timeStamp = myVideo.getCurrentTime();
+                let timeStart = myCaptions[i].getAttribute('data-start');
+                let timeEnd = myCaptions[i].getAttribute('data-end');
+                    if (timeStamp > timeStart & timeStamp < timeEnd){
+                        myCaptions[i].style.color = "#e5a01e";
+                    } else {
+                        myCaptions[i].style.color = "";
+                    }
+            }
+    };
+
+//click caption to move to video location
+
+    for (let i = 0; i < myCaptions.length; i += 1){
+        	myCaptions[i].addEventListener('click', (click) => {
+        		let timeClick = click.target.getAttribute("data-start");
+        		myVideo.setCurrentTime(timeClick);
+        	});
+          }
+
+});
+
+//myVideo.play();
